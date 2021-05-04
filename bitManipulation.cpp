4c9703512bc4b3bdef39720c45de87dfb4199ab3
Aman Singh <kdas@Amans-MacBook-Air.local>
2021-05-04 20:17:57 +0530

    Bit Manipulation Apni Kaksha

--- bitManipulation.cpp
+++ bitManipulation.cpp
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// watch youtube videos of bit Manipulation of Apni Kaksha
+
+
 int getBit(int n, int pos) {
 	return ((n & (1 << pos)) != 0);
 }
@@ -20,6 +23,65 @@ int updateBit(int n, int pos, int i) {
 	return (n | (i << pos));
 }
 
+bool isPowerof2(int n) {
+	return n && !(n & (n - 1));
+}
+
+int count1(int n) { // count number of 1 present in the binary of a Number
+	int count = 0;
+	while (n != 0) {
+		n = (n & (n - 1));
+		count++;
+	}
+	return count;
+}
+
+
+// No of Subsets of a set
+void subsets(char arr[], int n) {
+	for (int i = 0; i < (1 << n); i++) {
+		for (int j = 0; j < n; j++) {
+			if (getBit(i, j) == 1) {
+				cout << arr[j] << " ";
+			}
+		}
+		cout << "\n";
+	}
+}
+
+// Find one unique element in the given array given that only one unique element is present
+int unique1(int arr[], int n) {
+	int xorsum = 0;
+	for (int i = 0; i < n; i++) {
+		xorsum = xorsum ^ arr[i];
+	}
+	return xorsum;
+}
+
+// WAP to find 2 numbers in an array where all numbers except 2 are present twice.
+void unique2(int arr[], int n) {
+	int xorsum = 0;
+	for (int i = 0; i < n; i++) {
+		xorsum = xorsum ^ arr[i];
+	}
+	int xorsum1 = xorsum;
+	int pos;
+	for (int i = 0; i < n; i++) {
+		if (getBit(xorsum, i) == 1) {
+			pos = i;
+			break;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		if (getBit(arr[i], pos) == 1) {
+			xorsum = xorsum ^ arr[i];
+		}
+	}
+	cout << xorsum << "\n";
+
+	cout << (xorsum ^ xorsum1) << "\n";
+}
+
 
 int main() {
 #ifndef ONLINE_JUDGE
@@ -27,9 +89,15 @@ int main() {
 	freopen("output1.txt", "w", stdout);
 #endif
 
-	// cout << setBit(5, 1) << "\n";
-	// cout << clearBit(5, 2) << "\n";
-	cout << updateBit(5, 1, 1) << "\n";
+	int n;
+	cin >> n;
+
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	unique2(arr, n);
 
 	return 0;
 }
